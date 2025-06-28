e5f43f17fc19eb541a3ec3b0dbda4d079521e33c
Aiman Afiq bin Suradi <aimanafiq05@gmail.com>
2025-06-29 02:33:26 +0800

    added shadcn and typeorm

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({})],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ...config.get('database'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
