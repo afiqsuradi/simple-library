2077df1792ad2db1e143428669a20ae17b0e6c6d
Aiman Afiq bin Suradi <aimanafiq05@gmail.com>
2025-06-30 03:22:28 +0800

    run lint

--- apps/api/src/entity/user.entity.ts
+++ apps/api/src/entity/user.entity.ts
@@ -1,10 +1,10 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    Index
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
 } from 'typeorm';
 
 @Entity('users')
@@ -12,50 +12,50 @@ import {
 @Index('IDX_USERS_EMAIL', ['email'])
 @Index('IDX_USERS_IS_ACTIVE', ['isActive'])
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({
-        type: 'varchar',
-        unique: true,
-        nullable: false
-    })
-    username: string;
+  @Column({
+    type: 'varchar',
+    unique: true,
+    nullable: false,
+  })
+  username: string;
 
-    @Column({
-        type: 'varchar',
-        unique: true,
-        nullable: false
-    })
-    email: string;
+  @Column({
+    type: 'varchar',
+    unique: true,
+    nullable: false,
+  })
+  email: string;
 
-    @Column({
-        name: 'password_hash',
-        type: 'text',
-        nullable: false
-    })
-    passwordHash: string;
+  @Column({
+    name: 'password_hash',
+    type: 'text',
+    nullable: false,
+  })
+  passwordHash: string;
 
-    @Column({
-        name: 'is_active',
-        type: 'boolean',
-        default: true,
-        nullable: false
-    })
-    isActive: boolean;
+  @Column({
+    name: 'is_active',
+    type: 'boolean',
+    default: true,
+    nullable: false,
+  })
+  isActive: boolean;
 
-    @CreateDateColumn({
-        name: 'created_at',
-        type: 'timestamp',
-        nullable: false,
-    })
-    createdAt: Date;
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    nullable: false,
+  })
+  createdAt: Date;
 
-    @UpdateDateColumn({
-        name: 'updated_at',
-        type: 'timestamp',
-        nullable: false,
-        onUpdate: 'CURRENT_TIMESTAMP'
-    })
-    updatedAt: Date;
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    nullable: false,
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
 }
--- apps/api/src/migrations/1751224833005-create_user_table.ts
+++ apps/api/src/migrations/1751224833005-create_user_table.ts
@@ -1,20 +1,27 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUserTable1751224833005 implements MigrationInterface {
-    name = 'CreateUserTable1751224833005'
+  name = 'CreateUserTable1751224833005';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "email" character varying NOT NULL, "password_hash" text NOT NULL, "is_active" boolean NOT NULL DEFAULT true, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE INDEX "IDX_USERS_IS_ACTIVE" ON "users" ("is_active") `);
-        await queryRunner.query(`CREATE INDEX "IDX_USERS_EMAIL" ON "users" ("email") `);
-        await queryRunner.query(`CREATE INDEX "IDX_USERS_USERNAME" ON "users" ("username") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "email" character varying NOT NULL, "password_hash" text NOT NULL, "is_active" boolean NOT NULL DEFAULT true, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_USERS_IS_ACTIVE" ON "users" ("is_active") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_USERS_EMAIL" ON "users" ("email") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_USERS_USERNAME" ON "users" ("username") `,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_USERS_USERNAME"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_USERS_EMAIL"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_USERS_IS_ACTIVE"`);
-        await queryRunner.query(`DROP TABLE "users"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "public"."IDX_USERS_USERNAME"`);
+    await queryRunner.query(`DROP INDEX "public"."IDX_USERS_EMAIL"`);
+    await queryRunner.query(`DROP INDEX "public"."IDX_USERS_IS_ACTIVE"`);
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
 }
